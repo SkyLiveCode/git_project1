9b83f6f90ba9faa97a96724b5487c1b13fb050e5
SkyLiveCode <pongsaguy_2540@hotmail.com>
2024-06-28 13:53:09 +0700

    ห

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -23,5 +23,13 @@ router.get('/reset-password-direct', (req, res) => {
 });
 router.post('/reset-password-direct', authController.resetPasswordDirect);
 
+
+
+// กำหนดเส้นทาง GET สำหรับหน้า tableForAdapt เดลเอาออก
+router.get('/tableForAdapt', (req, res) => {
+  res.render('tableForAdapt');
+});
+
+
 // ส่งออกโมดูล router เพื่อให้สามารถใช้งานในไฟล์อื่นได้
 module.exports = router;
