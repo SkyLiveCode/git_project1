b655ccdeedcf8e7ee530bf58f3f836b3df235b76
SkyLiveCode <pongsaguy_2540@hotmail.com>
2024-06-30 08:03:19 +0700

    w

--- routes/gateEquipmentRoutes.js
+++ routes/gateEquipmentRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const gateEquipmentController = require('../controllers/gateEquipmentController');
 
 // เส้นทางสำหรับรับ id
-router.get('/html/pages-gate_equipment_by_id/:id', (req, res) => {
-    req.session.id_hospital = req.params.id; // เก็บ id_hospital ใน session
-    res.redirect('/html/pages-gate_equipment'); // เปลี่ยนเส้นทางไปยังหน้าข้อมูลอุปกรณ์ทางการแพทย์
+router.get('/html/pages-gate_equipment_by_id/:equipment_id/:id_hospital/:id_categories', (req, res) => {
+    req.session.equipment_id = req.params.equipment_id;
+    req.session.id_hospital = req.params.id_hospital;
+    req.session.id_categories = req.params.id_categories;
+    res.redirect('/html/pages-gate_equipment'); // เปลี่ยนเส้นทางไปยังหน้า Calculate 1
 });
 
 // เส้นทางสำหรับรับ id_hospital
